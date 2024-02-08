8f6c7320ec4e87e822c6861445de2fd96947a04a
Schenier Lopez <schnierlopez@gmail.com>
2024-02-08 19:41:34 +0100

    fix: Remove unused trait

--- src/Entrypoint/Controller/BeerRetrieverController.php
+++ src/Entrypoint/Controller/BeerRetrieverController.php
@@ -6,12 +6,9 @@ use App\Application\Query\BeerRetrieverQuery;
 use App\Shared\Domain\Command\MessageAppBusInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Messenger\HandleTrait;
 
 class BeerRetrieverController extends AbstractController
 {
-    use HandleTrait;
-
     public function __construct(private readonly MessageAppBusInterface $bus) {}
 
     public function __invoke(): JsonResponse
